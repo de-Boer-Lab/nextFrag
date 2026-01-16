e2d218cc60284f8282bef8e5368f0f3c1ca444c0
Justin Qian <jq3752@gmail.com>
2026-01-16 13:35:29 -0800

    max expression and ISM selection methods

---
+++ sequence_selection/bio_informed.py
@@ -0,0 +1,180 @@
+import torch
+import pandas as pd
+import numpy as np
+import itertools
+import argparse
+from tqdm import tqdm
+from models.model_utils import load_model
+from models.dl_utils import prepare_dataloader
+
+def max_expression(species: str,
+                    arch: str,
+                    seed: int,
+                    num_selected: int,
+                    lowest: bool=False):
+    DATA_ROOT=""
+    data_path=f'{DATA_ROOT}/{species}/round_0/common/pool.txt'
+    SEQSIZE=200 if species=='human' else 150
+    BATCH_SIZE=2048
+    generator = torch.Generator()
+    generator.manual_seed(seed)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    # remember original=True for human
+    original = species == 'human'
+    model=load_model(species=species,arch=arch,al_method='common',seed=seed,round='0',original=original)
+    model.to(device).eval()
+    dl=prepare_dataloader(data_path,
+                        seqsize=SEQSIZE,
+                        species=species,
+                        batch_size=BATCH_SIZE,
+                        shuffle=False,
+                        generator=generator)
+    
+    df=pd.read_csv(data_path,header=None,sep='\t')
+    num_seqs=len(df)
+
+    all_preds=[]
+    with torch.inference_mode():
+        for batch in dl:
+            X=batch['x'].to(device)
+            pred=model.predict(X)
+            all_preds.append(pred.cpu().numpy())
+    all_preds=np.concatenate(all_preds)
+    all_preds=all_preds.reshape(2,num_seqs)
+    all_preds=np.sum(all_preds,axis=0)/2 
+    df['pred']=all_preds
+    df_new=df.sort_values(by=['pred'],ascending=lowest)
+    result=df_new[:num_selected]
+    result.to_csv(f'{DATA_ROOT}/{species}_maxpred_expr_{seed}.tsv',sep='\t',index=None,header=None,columns=[0,1])
+
+def ism(file_path: str, out_path: str, dataset: str, job_id: int, seqs_per_job: int=1_000_000, window_sz: int=6, arch: str='rnn'):
+    df = pd.read_csv(file_path, header=None, sep='\t')
+    df.columns=['seq','expr']
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    if dataset=='human':
+        batch_size=8
+        start_pos=0
+        end_pos=200
+        seqsize=200
+        original = True
+        df= df[df['seq'].str.len()==seqsize]
+    else: # yeast
+        batch_size=16
+        start_pos=57
+        end_pos=137
+        seqsize=150
+        original = False
+        left_flank="AGTGCTAGCAGGAATGATGCAAAAGGTTCCCGATTCGAAC"
+        df= df[df['seq'].str[:len(left_flank)]==left_flank]
+    df = df.iloc[job_id*seqs_per_job:(job_id+1)*seqs_per_job]
+
+    model=load_model(species=dataset,arch=arch,al_method='common',seed=1,round='0',original=original)
+    model.to(device).eval()
+
+    attrs = []
+    buffer = []
+    for row in tqdm(df.itertuples()):
+        seq = row.seq.upper()
+        X=seq2tensor(seq,dataset)
+        buffer.append(X)
+        if len(buffer)>=batch_size:
+            X=torch.cat(buffer,dim=0)
+            y, y_ism = saturation_mutagenesis(model=model,
+                                              X=X,
+                                              start=start_pos,
+                                              end=end_pos,
+                                              device=device)
+            
+            y_attr = y_ism - y[:, None, None] 
+            attrs.append(y_attr.squeeze().cpu().detach().numpy())
+            buffer=[]
+    if buffer:
+        X=torch.cat(buffer,dim=0)
+        y, y_ism = saturation_mutagenesis(model=model,
+                                          X=X,
+                                          start=start_pos,
+                                          end=end_pos,
+                                          device=device)
+        
+        y_attr = y_ism - y[:, None, None] 
+        attrs.append(y_attr.squeeze().cpu().detach().numpy())
+    
+    attrs=np.concatenate(attrs,axis=0)
+    result=compute_attributions(attrs,window_sz)
+    out_df = pd.concat([df[["seq"]].reset_index(drop=True),
+                        pd.DataFrame(result,columns=['mean','max','window max'])],axis=1)
+    out_df.to_csv(out_path,sep='\t',index=None)
+
+def _edit_distance_one(X, start, end): # X.shape==(C,L)
+    if end < 0:
+        end = X.shape[-1] + end + 1
+    X_ = X.repeat((end-start)*3, 1, 1)
+
+    coords = itertools.product(range(start, end),range(4))
+    _next=0
+    for pos, mut in coords:
+        if X[mut,pos]==1: # no mutation
+            continue
+        X_[_next, :4, pos] = 0
+        X_[_next, mut, pos] = 1
+        _next+=1
+    return X_
+
+def saturation_mutagenesis(model, X, start=0, end=-1, device='cuda'):
+    N, C, L = X.shape
+    if end < 0:
+        end = L + end + 1
+    n_mut = (end - start) * 3
+    
+    X_mut = [_edit_distance_one(X[i], start, end) for i in range(N)]
+    X_mut = torch.cat(X_mut, dim=0)   # (N * n_mut, C, L)
+    X_all = torch.cat((X,X_mut),dim=0)
+    
+    model = model.to(device).eval()
+    dtype = next(model.parameters(), X).dtype
+    
+    with torch.inference_mode():
+        y = model.predict(X_all.to(device).type(dtype)).cpu()
+
+    y0 = y[:N]
+    y_hat = y[N:].view(N,end-start, 3, *y.shape[1:])
+    return y0, y_hat
+
+def one_hot_encode(seq):
+    mapping = {'A': [1, 0, 0, 0],
+            'G': [0, 1, 0, 0],
+            'C': [0, 0, 1, 0],
+            'T': [0, 0, 0, 1]}
+    return [mapping[base] for base in seq]
+
+def seq2tensor(seq,dataset):
+    ohe_seq = one_hot_encode(seq)
+    rev_values = [0] * len(ohe_seq)
+    is_singletons = [0] * len(ohe_seq)
+    if dataset=='yeast':
+        encoded = [list(ohe) + [rev] + [is_singleton] 
+                    for ohe, rev, is_singleton in zip(ohe_seq, rev_values, is_singletons)]
+    else:
+        encoded = [list(ohe) + [rev] for ohe, rev in zip(ohe_seq, rev_values)]
+    X = torch.Tensor(encoded).type(torch.float32)
+    X = X.unsqueeze(0)
+    X = torch.transpose(X, 2, 1)
+    return X
+def compute_attributions(attrs,window_sz):
+    attrs=-attrs.mean(axis=2) # average across substitutions
+    N,L=attrs.shape
+    
+    _mean=attrs.mean(axis=1)
+    maxpos=np.abs(attrs).argmax(axis=1)
+    _max=attrs[np.arange(N),maxpos]
+    
+    windows = np.lib.stride_tricks.sliding_window_view(attrs, window_sz, axis=1)
+    window_sums = windows.sum(axis=2)
+    idx = np.abs(window_sums).argmax(axis=1)
+    max_window = window_sums[np.arange(N), idx]
+
+    result=np.stack((_mean,_max,max_window),axis=-1)
+    return result
+
+
